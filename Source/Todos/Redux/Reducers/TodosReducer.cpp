3fd278d52d62a0043b75c4178213b6004abf5426
essem <soomin@gmail.com>
2018-03-22 14:18:30 +0900

    Add test code for reducers

--- Source/Todos/Redux/Reducers/TodosReducer.cpp
+++ Source/Todos/Redux/Reducers/TodosReducer.cpp
@@ -76,3 +76,84 @@ const UTodoStateArray* TodosReducer(const UTodoStateArray* State, const UAction*
 
 	return State;
 }
+
+#if WITH_EDITOR
+
+#define ASSERT_EQUAL(x,y) if ((x)!=(y)) { return false; }
+#define ASSERT_EQUAL_TEXT(x,y) if (!(x).EqualTo(y)) { return false; }
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTodosReducerInitialStateTest, "Redux.TodosReducer.InitialState", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+bool FTodosReducerInitialStateTest::RunTest(const FString& Parameters)
+{
+	const UTodoStateArray* State = TodosReducer(nullptr, nullptr);
+	ASSERT_EQUAL(State->GetArray().Num(), 0);
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTodosReducerAddTodoTest, "Redux.TodosReducer.AddTodo", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+bool FTodosReducerAddTodoTest::RunTest(const FString& Parameters)
+{
+	const UTodoStateArray* State = UTodoStateArray::Create(TArray<UTodoState*>());
+	UAddTodoAction* Action = NewObject<UAddTodoAction>();
+	Action->Text = FText::FromString("Run the tests");
+	Action->Id = 0;
+
+	const UTodoStateArray* NewState = TodosReducer(State, Action);
+	ASSERT_EQUAL(NewState->GetArray().Num(), 1);
+	ASSERT_EQUAL_TEXT(NewState->GetArray()[0]->Text->GetText(), FText::FromString("Run the tests"));
+	ASSERT_EQUAL(NewState->GetArray()[0]->bCompleted, false);
+	ASSERT_EQUAL(NewState->GetArray()[0]->Id, 0);
+
+	State = NewState;
+	Action = NewObject<UAddTodoAction>();
+	Action->Text = FText::FromString("Use Redux");
+	Action->Id = 1;
+
+	NewState = TodosReducer(State, Action);
+	ASSERT_EQUAL(NewState->GetArray().Num(), 2);
+	ASSERT_EQUAL_TEXT(NewState->GetArray()[0]->Text->GetText(), FText::FromString("Run the tests"));
+	ASSERT_EQUAL(NewState->GetArray()[0]->bCompleted, false);
+	ASSERT_EQUAL(NewState->GetArray()[0]->Id, 0);
+	ASSERT_EQUAL_TEXT(NewState->GetArray()[1]->Text->GetText(), FText::FromString("Use Redux"));
+	ASSERT_EQUAL(NewState->GetArray()[1]->bCompleted, false);
+	ASSERT_EQUAL(NewState->GetArray()[1]->Id, 1);
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTodosReducerToggleTodoTest, "Redux.TodosReducer.ToggleTodo", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+bool FTodosReducerToggleTodoTest::RunTest(const FString& Parameters)
+{
+	TArray<UTodoState*> NewArray;
+
+	UTodoState* TodoState = NewObject<UTodoState>();
+	TodoState->Id = 0;
+	TodoState->Text = UStateText::Create(FText::FromString("Run the tests"));
+	TodoState->bCompleted = false;
+	NewArray.Add(TodoState);
+
+	TodoState = NewObject<UTodoState>();
+	TodoState->Id = 1;
+	TodoState->Text = UStateText::Create(FText::FromString("Use Redux"));
+	TodoState->bCompleted = false;
+	NewArray.Add(TodoState);
+
+	const UTodoStateArray* State = UTodoStateArray::Create(MoveTemp(NewArray));
+
+	UToggleTodoAction* Action = NewObject<UToggleTodoAction>();
+	Action->Id = 1;
+
+	const UTodoStateArray* NewState = TodosReducer(State, Action);
+	ASSERT_EQUAL(NewState->GetArray().Num(), 2);
+	ASSERT_EQUAL_TEXT(NewState->GetArray()[0]->Text->GetText(), FText::FromString("Run the tests"));
+	ASSERT_EQUAL(NewState->GetArray()[0]->bCompleted, false);
+	ASSERT_EQUAL(NewState->GetArray()[0]->Id, 0);
+	ASSERT_EQUAL_TEXT(NewState->GetArray()[1]->Text->GetText(), FText::FromString("Use Redux"));
+	ASSERT_EQUAL(NewState->GetArray()[1]->bCompleted, true);
+	ASSERT_EQUAL(NewState->GetArray()[1]->Id, 1);
+
+	return true;
+}
+
+#endif
--- Source/Todos/Redux/Reducers/VisibilityFilterReducer.cpp
+++ Source/Todos/Redux/Reducers/VisibilityFilterReducer.cpp
@@ -26,3 +26,31 @@ const UStateString* VisibilityFilterReducer(const UStateString* State, const UAc
 
 	return State;
 }
+
+#if WITH_EDITOR
+
+#define ASSERT_EQUAL(x,y) if ((x)!=(y)) { return false; }
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FVisibilityFilterReducerInitialStateTest, "Redux.VisibilityFilterReducer.InitialState", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+bool FVisibilityFilterReducerInitialStateTest::RunTest(const FString& Parameters)
+{
+	const UStateString* NewState = VisibilityFilterReducer(nullptr, nullptr);
+	ASSERT_EQUAL(NewState->GetString(), TEXT("SHOW_ALL"));
+
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FVisibilityFilterReducerSetVisibilityFilterTest, "Redux.VisibilityFilterReducer.SetVisibilityFilter", EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+bool FVisibilityFilterReducerSetVisibilityFilterTest::RunTest(const FString& Parameters)
+{
+	const UStateString* State = UStateString::Create(TEXT("SHOW_ALL"));
+	USetVisibilityFilterAction* Action = NewObject<USetVisibilityFilterAction>();
+	Action->Filter = TEXT("SHOW_ACTIVE");
+
+	const UStateString* NewState = VisibilityFilterReducer(State, Action);
+	ASSERT_EQUAL(NewState->GetString(), TEXT("SHOW_ACTIVE"));
+
+	return true;
+}
+
+#endif
